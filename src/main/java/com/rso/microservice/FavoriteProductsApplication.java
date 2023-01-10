9fb17465ad7971e05e5ef91157e0318cdb032257
Aljaž Šmaljcelj <aljaz.smaljcelj7@gmail.com>
2023-01-10 10:31:00 +0100

    new logging, removed namespace and new config server URL, added refreshScope

--- src/main/java/com/rso/microservice/FavoriteProductsApplication.java
+++ src/main/java/com/rso/microservice/FavoriteProductsApplication.java
@@ -2,8 +2,10 @@ package com.rso.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication(scanBasePackages = {"com.rso.microservice", "com.rso.microservice.api", "com.rso.microservice.api.mapper"})
+@RefreshScope
 public class FavoriteProductsApplication {
 
 	public static void main(String[] args) {
